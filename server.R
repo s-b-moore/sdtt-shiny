cfcd5d509c2aed4b1ba671ba0ed1eb51bbab74e5
Stuart Moore <mooresb.ac@gmail.com>
2023-05-29 22:34:03 +0100

    tidied server file and added Rproject

--- server.R
+++ server.R
@@ -1,17 +1,12 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# === define server logic =====================================================
 
+# --- load packages -----------------------------------------------------------
 library(shiny)
 
-# Define server logic required to draw a histogram
+# --- server ------------------------------------------------------------------
 function(input, output, session) {
 
+  # --- automatically assign outcomes and display table -----------------------
   output$contents <- renderTable({
     inFile <- input$dataInput
     
